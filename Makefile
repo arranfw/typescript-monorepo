48d5817b53be5adff355e3f6315c8f538f31476c
arranfw <arran@freshworks.io>
2022-09-26 10:22:13 -0600

    repo: initial setup, lint, format, test, build

---
+++ Makefile
@@ -0,0 +1,17 @@
+dev:
+	@yarn dev
+
+build: 
+	@yarn build
+
+lint: 
+	@yarn lint
+
+test: 
+	@yarn test
+
+format:
+	@yarn format
+
+install:
+	@yarn 
--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,9 +1,29 @@
-import { Button } from "ui";
+import { useEffect, useState } from 'react';
+import { Button } from 'ui';
+
+const useFetchHello = () => {
+  const [hello, setHello] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(`http://localhost:4000`);
+      const data = await response.json();
+      setHello(data.hello);
+    };
+
+    fetchData();
+  }, []);
+
+  return { hello };
+};
 
 export default function Web() {
+  const { hello } = useFetchHello();
+
   return (
     <div>
       <h1>Web</h1>
+      <p>{hello}</p>
       <Button />
     </div>
   );
