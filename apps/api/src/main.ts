5343a4d05e35e4dd44a95f245d9c12d2cdbf1c92
arranfw <arran@freshworks.io>
2022-10-26 08:35:25 -0700

    feat: api versioning

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,3 +1,4 @@
+import { VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
@@ -8,6 +9,10 @@ async function bootstrap() {
     logger: customLogger,
   });
   app.enableCors();
+  app.enableVersioning({
+    defaultVersion: '1',
+    type: VersioningType.URI,
+  });
   await app.listen(4000);
 }
 bootstrap();
